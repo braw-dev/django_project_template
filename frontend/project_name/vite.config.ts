7ffdeb5585371c2d0d2b34bcf8ad2fb5820de79f
Ewan Jones <ewan@climacrux.com>
2025-06-20 22:22:38 +0200

    feat: integrate Vite for asset management and update settings for static files

--- frontend/project_name/vite.config.ts
+++ frontend/project_name/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/static/",
+  build: {
+    manifest: "manifest.json",
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        tailwind: "src/tailwind.css",
+      }
+    }
+  },
   plugins: [react()],
 })
